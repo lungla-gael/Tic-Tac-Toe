eda6e085483e43a2be09516681fdc4b96e07461d
lunglagael <lunglagael@gmail.com>
2020-06-11 20:44:58 +0100

    implemented some global variables correctly

--- main.rb
+++ main.rb
@@ -9,8 +9,8 @@ def display_board(map)
     print "c \t #{map["c1"]}  | \t #{map["c2"]}  | \t #{map["c3"]} "; print "\n\n"
 end
 
-player1 = nil
-player2 = nil
+$player1 = nil
+$player2 = nil
 chosen_characters = []
 puts "enter players' names and characters (X/O)"
 print "\n"
@@ -23,7 +23,7 @@ print "\n"
         unless chosen_characters.include?(character)
             if character.upcase.eql?("X") || character.upcase.eql?("O")
                 chosen_characters.push(character)
-                a + 1 === 1? player1 = Player.new(name,character) : player2 = Player.new(name,character)
+                a === 0? $player1 = Player.new(name,character) : $player2 = Player.new(name,character)
                 break       
             end            
         end      
@@ -31,38 +31,38 @@ print "\n"
 }
 
 ALL_SLOTS = "a1b1c1a2b2c2a3b3c3"
-def round(player1,player2)
-    player1.to_default
-    player2.to_default
-    board_map = {}
-    board_map["filled"] = 0
-    display_board(board_map)
+def round
+    $player1.to_default
+    $player2.to_default
+    $board_map = {}
+    $board_map["filled"] = 0
+    display_board($board_map)
     finish = false
     
     puts "select a slot to mark e.g a1"
     print "\n"
-    def play(player,board_map)
+    def play(player)
         print "#{player.name} mark a slot : "
         slot = gets.chomp
         if ALL_SLOTS.include?(slot)
-            if board_map.has_key?(slot)
+            if $board_map.has_key?(slot)
                 puts "OOPS! slot #{slot} is occupied"
-                play(player,board_map)   
+                play(player)   
             else
-                board_map[slot] = player.character
-                board_map["filled"] += 1
+                $board_map[slot] = player.character
+                $board_map["filled"] += 1
                 player.position = slot
-                display_board(board_map) 
+                display_board($board_map) 
             end        
         else
             puts "Sorry slot #{slot} does not exist on the board"
-            play(player,board_map,)
+            play(player)
         end
     end
 
-    def check_winner?(player,board_map)
-        play(player,board_map)
-        if board_map["filled"].eql?(9)
+    def check_winner?(player)
+        play(player)
+        if $board_map["filled"].eql?(9)
             puts "\t\t Its a tie! Nobody Wins!" 
             true
         else
@@ -76,18 +76,18 @@ def round(player1,player2)
     end
     
     until finish === true
-        finish = check_winner?(player1,board_map)
+        finish = check_winner?($player1)
         break if finish === true
-        finish = check_winner?(player2,board_map)
+        finish = check_winner?($player2)
         break if finish === true
     end    
     
 end
 
-again = "y"
-while again.eql?("y")
-    round(player1,player2)
+play_again = "y"
+while play_again.eql?("y")
+    round
     print "\t you guys wanna play again? y/n : "
-    again = gets.chomp
+    play_again = gets.chomp
     print "\n"    
 end
